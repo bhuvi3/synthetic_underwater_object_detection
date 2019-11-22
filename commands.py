d02996b3b76fbf84157f9a74f81d39d3dc9536e4
msbhuvan <msbhuvan@uw.edu>
2019-11-22 00:57:29 -0800

    Update commands.

--- commands.py
+++ commands.py
@@ -13,18 +13,22 @@ docker cp path_in_local a0bb7eeed020:/projects/
 
 # All scripts run from the code directory.
 
+### Create data.
 # Yolo data synthetic renderer.
 python yolo_data_renderer.py --background-dir ../data/underwater_background/unsplash/unsplash_underwater_collection --object-model-file ../panda3d_models/Torus.egg --num-scenes 1000 --out-dir ../data/darknet_datasets/unsplash_rendered_test/darknet_images_labels --max-objects 2 --custom-start-index -2
 
-# Darknet
 
-### Data preparation:
+# Manual: Check image names.
+# Manual: Draw bounding box and verify labels.
+
+### Darknet
+# Data preparation:
 python darknet_dataset_creator.py --dataset-name unsplash_rendered_test --data-dir ../data/darknet_datasets/unsplash_rendered_test/darknet_images_labels --classes torus --out-dir ../data/darknet_datasets/unsplash_rendered_test
 
-### Manually add the config file like ("yolov3-tiny.cfg"), and have the pretrained weights from "darknet53.conv.74". Set max_batches to epochs/
+# Manually add the config file like ("yolov3-tiny.cfg"), and have the pretrained weights from "darknet53.conv.74". Set max_batches to epochs/
 
-### Training.
+# Training.
 ./darknet detector train /home/bhuvan/Projects/underwater_synthetic_image_recognition/data/darknet_datasets/unsplash_rendered_test/unsplash_rendered_test.data /home/bhuvan/Projects/underwater_synthetic_image_recognition/data/darknet_datasets/unsplash_rendered_test/yolov3-tiny.cfg darknet53.conv.74
 
-### Testing.
+# Testing.
 ./darknet detector test /home/bhuvan/Projects/underwater_synthetic_image_recognition/data/darknet_datasets/unsplash_rendered_test/unsplash_rendered_test.data  /home/bhuvan/Projects/underwater_synthetic_image_recognition/data/darknet_datasets/unsplash_rendered_test/yolov3-tiny.cfg /home/bhuvan/Projects/underwater_synthetic_image_recognition/data/darknet_datasets/unsplash_rendered_test/darknet_backup/yolov3-tiny_last.weights /home/bhuvan/Projects/underwater_synthetic_image_recognition/data/darknet_datasets/unsplash_rendered_test/darknet_images_labels/almos-bechtold-A6TVJBNA-z4-unsplash-107.jpg
